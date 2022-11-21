5f6f57372b224177f09c746d73f18b13ea1337f2
andresindlp <35582438+andresindlp@users.noreply.github.com>
2022-11-21 10:15:43 +0100

    Create poke.py

---
+++ poke.py
@@ -0,0 +1,19 @@
+# Descarga listado pokemon en formato JSON
+# # Tareas buscar en Internet para que sirve las librerias request y python
+# # Opcional: Visualizar un listado con sólo los nombres de los pokemon
+# # consultando esta página https://daviddelatorre.me/json-con-python-obtener-los-datos/
+
+import requests
+import json
+
+url="https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json"
+response = requests.get(url)
+data = json.loads(response.text)
+
+print(data["pokemon"])
+
+
+##########################
+for i in data["pokemon"]:
+    print(i["name"])
+##########################
