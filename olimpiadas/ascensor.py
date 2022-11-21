45ef1a4f81acea9b73c1b0c67d2b4ee716b51e03
andresindlp <35582438+andresindlp@users.noreply.github.com>
2022-11-21 10:00:16 +0100

    Create ascensor.py

---
+++ olimpiadas/ascensor.py
@@ -0,0 +1,33 @@
+global panta, pisos_validos, personas, avisos, entrada, ultimo_piso
+planta = 0
+pisos_validos = []
+personas = 0
+avisos = "AVISOS\n"
+entrada = input("Abriendo puertas. Pueden pasar ... ").split(" ")
+ultimo_piso = ""
+
+
+# pisos = 20
+# max personas = 6
+
+while personas <= 6:
+    for i in entrada:
+        if int(i) != -1:
+            if 0 < int(i) < 20:
+                pisos_validos.append(i)
+                personas += 1
+                ultimo_piso = i
+            else:
+                avisos += ("El usuario que quiere ir al piso " + i + " no puede subir al ascensor\n")
+                ultimo_piso = i
+        else:
+            break
+else:
+    avisos += "El usuario que quiere ir al piso " + ultimo_piso + " ya no cabe\n"
+
+print(avisos)
+        
+    
+
+
+    
