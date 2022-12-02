1da763acf6aa8ce93acd92a7538b8f44f84b5651
andresindlp <35582438+andresindlp@users.noreply.github.com>
2022-12-02 10:35:57 +0100

    Update ascensor.py

--- olimpiadas/ascensor.py
+++ olimpiadas/ascensor.py
@@ -1,34 +1,38 @@
 global panta, pisos_validos, personas, avisos, entrada, ultimo_piso
 planta = 0
-pisos_validos = []
-personas = 0
-avisos = ["AVISOS"]
-entrada = input("Abriendo puertas. Pueden pasar ... ").split(" ")
-pisos_invalidos = []
-
-
+pisos_validos, pisos_invalidos, avisos, personas, entrada = [], [], ["\nAVISOS"], 1, input("Abriendo puertas. Pueden pasar ... ").split(" ")
 
 # pisos = 20
 # max personas = 6
 
-while personas <= 6:
-    for i in entrada:
-        if int(i) != -1:
-            if 0 < int(i) < 20:
+for i in entrada:
+    if int(i) != -1:
+        if 0 < int(i) < 21:
+            if len(pisos_validos) <= 5:
                 pisos_validos.append(i)
-                personas += 1
-                ultimo_piso = i
+
             else:
-                avisos += ("El usuario que quiere ir al piso " + i + " no puede subir al ascensor\n")
-                ultimo_piso = i
+                avisos.append("- El usuario que quiere ir al piso " + i + " ya no cabe\n")
         else:
-            break
-else:
-    avisos.append("El usuario que quiere ir al piso " + ultimo_piso + " ya no cabe")
+            avisos.append("- El piso " + i + " no existe\n")
 
 print("\n".join(avisos))
+
+print("Cerrando puertas. Estamos en el piso 0")
+
+for piso in pisos_validos:
+    if int(piso) > int(planta):
+        print("Subiendo a una persona la planta", str(piso))
+        planta = piso
+    elif int(piso) == int(planta):
+        print("\tLa siguiente persona puede bajar")
+        planta = piso
+    else:
+        print("Bajando a una persona la planta", str(piso))
+        planta = piso
+
+
+
         
     
 
-
-    
